9f0456431acc73262f04f1959ed265d0979e5292
Lindos Mahadi <lindosmahadi@gmail.com>
2017-11-25 17:02:38 +0600

    Zoo App MVC model 2 crud ok

---
+++ ZooApp/Controllers/Animal1Controller.cs
@@ -0,0 +1,111 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using ZooApp.DAL;
+using ZooApp.Models;
+
+namespace ZooApp.Controllers
+{
+    public class Animal1Controller : Controller
+    {
+        private ZooContext db = new ZooContext();
+        AnimalService service = new AnimalService();
+        // GET: Animal
+        public ActionResult Index()
+        {
+            
+            var viewAnimals = service.GetAnimals();
+
+            return View(viewAnimals);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Animal animal)
+        {
+            //save
+            //if (ModelState.IsValid)
+            //{
+            //    bool save = service.Save(animal);
+            //    return RedirectToAction("Index");
+            //}
+            bool save = service.Save(animal);
+            return RedirectToAction("Index");
+            return View(animal);
+        }
+
+        public ActionResult Details(int id)
+        {
+            ViewAnimal animal = service.GetAnimal(id);
+            return View(animal);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Animal animal = service.GetDbAnimal(id);
+            return View(animal);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Animal animal)
+        {
+            bool save = service.Update(animal);
+            return RedirectToAction("Index");
+            return View(animal);
+        }
+
+        //[HttpGet]
+        //public ActionResult Delete()
+        //{
+
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult Delete(Animal animal)
+        //{
+        //    bool save = service.Delete(animal);
+        //    return RedirectToAction("Index");
+        //    //return View(animal);
+        //}
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animal animal = db.Animals.Find(id);
+            if (animal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animal);
+        }
+
+        // POST: /Animal/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Animal animal = db.Animals.Find(id);
+            db.Animals.Remove(animal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ZooApp/Models/AnimalService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using ZooApp.DAL;
+
+namespace ZooApp.Models
+{
+    public class AnimalService
+    {
+        ZooContext db = new ZooContext();
+        public List<ViewAnimal> GetAnimals()
+        {
+            List<Animal> animals = db.Animals.ToList();
+
+            List<ViewAnimal> viewAnimals = new List<ViewAnimal>();
+
+            foreach (Animal animal in animals)
+            {
+                ViewAnimal viewAnimal = new ViewAnimal()
+                {
+                    Id = animal.Id,
+                    Origin = animal.Origin,
+                    Quantity = animal.Quantity,
+                    Food = animal.Food,
+                    Name = animal.Name
+                };
+                viewAnimals.Add(viewAnimal);
+            }
+            return viewAnimals;
+        }
+
+        public ViewAnimal GetAnimal(int id)
+        {
+            Animal animal = new Animal();
+            db.Animals.Find(id);
+            return new ViewAnimal()
+            {
+                Id = animal.Id,
+                Origin = animal.Origin,
+                Quantity = animal.Quantity,
+                Food = animal.Food,
+                Name = animal.Name
+            };
+        }
+
+        public bool Save(Animal animal)
+        {
+         Animal add = db.Animals.Add(animal);
+            db.SaveChanges();
+            return true;
+        }
+
+        public bool Update(Animal animal)
+        {
+            db.Entry(animal).State = EntityState.Modified;
+            db.SaveChanges();
+            return true;
+        }
+        public bool Delete(Animal animal)
+        {
+            Animal dbAnimal = db.Animals.Find(animal.Id);
+            db.SaveChanges();
+            return true;
+        }
+
+        public Animal GetDbAnimal(int id)
+        {
+            return db.Animals.Find(id);
+        }
+    }
+}
