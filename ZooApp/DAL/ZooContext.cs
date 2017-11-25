af869247e4e53d81b400d0bedd1d0aaca4d22d0d
Lindos Mahadi <lindosmahadi@gmail.com>
2017-11-25 14:31:58 +0600

    Initial Commit Zoo1

---
+++ ZooApp/DAL/ZooContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using ZooApp.Models;
+
+namespace ZooApp.DAL
+{
+    public class ZooContext : DbContext
+    {
+        public ZooContext() : base("ZooContext")
+        {
+
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
